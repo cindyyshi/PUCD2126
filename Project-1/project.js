4f791a28e2734479ebb7b5ac1a48b1f7d4865b1e
cindyyshi <31453458+cindyyshi@users.noreply.github.com>
2018-04-24 19:13:28 -0400

    Add files via upload

---
+++ Project-1/project.js
@@ -0,0 +1,321 @@
+
+star1.addEventListener("click", handleSt1Click)
+
+function handleSt1Click() {
+	flower1.style.display = "block";
+	star1.style.display = "none";
+}
+
+flower1.addEventListener("click", handleFl1Click)
+
+function handleFl1Click() {
+	flower1.style.display = "none";
+	star1.style.display = "block";
+}
+
+
+flower1.addEventListener("mouseover", flower1Function);
+	
+flower1.addEventListener("mouseout", flower1Function1);
+	
+	function flower1Function(){
+		flower1.style.animationDuration= "4s";
+		flower1.style.animationName = "spin";
+		flower1.style.animationIterationCount = "infinite";
+		flower1.style.animationPlayState = "running";
+		flower1.style.animationTimingFunction = "linear";
+	}
+	function flower1Function1(){
+		flower1.style.animationPlayState = "paused";
+	}
+
+star2.addEventListener("click", handleSt2Click)
+
+function handleSt2Click() {
+	flower2.style.display = "block";
+	star2.style.display = "none";
+}
+
+flower2.addEventListener("click", handleFl2Click)
+
+function handleFl2Click() {
+	flower2.style.display = "none";
+	star2.style.display = "block";
+}
+
+flower2.addEventListener("mouseover", flower2Function);
+	
+flower2.addEventListener("mouseout", flower2Function2);
+	
+	function flower2Function(){
+		flower2.style.animationDuration= "4s";
+		flower2.style.animationName = "spin";
+		flower2.style.animationIterationCount = "infinite";
+		flower2.style.animationPlayState = "running";
+		flower2.style.animationTimingFunction = "linear";
+	}
+	function flower2Function2(){
+		flower2.style.animationPlayState = "paused";
+	}
+
+star3.addEventListener("click", handleSt3Click)
+
+function handleSt3Click() {
+	flower3.style.display = "block";
+	star3.style.display = "none";
+}
+
+flower3.addEventListener("click", handleFl3Click)
+
+function handleFl3Click() {
+	flower3.style.display = "none";
+	star3.style.display = "block";
+}
+
+flower3.addEventListener("mouseover", flower3Function);
+	
+flower3.addEventListener("mouseout", flower3Function3);
+	
+	function flower3Function(){
+		flower3.style.animationDuration= "4s";
+		flower3.style.animationName = "spin";
+		flower3.style.animationIterationCount = "infinite";
+		flower3.style.animationPlayState = "running";
+		flower3.style.animationTimingFunction = "linear";
+	}
+	function flower3Function3(){
+		flower3.style.animationPlayState = "paused";
+	}
+
+
+star4.addEventListener("click", handleSt4Click)
+
+function handleSt4Click() {
+	flower4.style.display = "block";
+	star4.style.display = "none";
+}
+
+flower4.addEventListener("click", handleFl4Click)
+
+function handleFl4Click() {
+	flower4.style.display = "none";
+	star4.style.display = "block";
+}
+
+flower4.addEventListener("mouseover", flower4Function);
+	
+flower4.addEventListener("mouseout", flower4Function4);
+	
+	function flower4Function(){
+		flower4.style.animationDuration= "4s";
+		flower4.style.animationName = "spin";
+		flower4.style.animationIterationCount = "infinite";
+		flower4.style.animationPlayState = "running";
+		flower4.style.animationTimingFunction = "linear";
+	}
+	function flower4Function4(){
+		flower4.style.animationPlayState = "paused";
+	}
+
+star5.addEventListener("click", handleSt5Click)
+
+function handleSt5Click() {
+	flower5.style.display = "block";
+	star5.style.display = "none";
+}
+
+flower5.addEventListener("click", handleFl5Click)
+
+function handleFl5Click() {
+	flower5.style.display = "none";
+	star5.style.display = "block";
+}
+
+flower5.addEventListener("mouseover", flower5Function);
+	
+flower5.addEventListener("mouseout", flower5Function5);
+	
+	function flower5Function(){
+		flower5.style.animationDuration= "4s";
+		flower5.style.animationName = "spin";
+		flower5.style.animationIterationCount = "infinite";
+		flower5.style.animationPlayState = "running";
+		flower5.style.animationTimingFunction = "linear";
+	}
+	function flower5Function5(){
+		flower5.style.animationPlayState = "paused";
+	}
+
+star6.addEventListener("click", handleSt6Click)
+
+function handleSt6Click() {
+	flower6.style.display = "block";
+	star6.style.display = "none";
+}
+
+flower6.addEventListener("click", handleFl6Click)
+
+function handleFl6Click() {
+	flower6.style.display = "none";
+	star6.style.display = "block";
+}
+
+flower6.addEventListener("mouseover", flower6Function);
+	
+flower6.addEventListener("mouseout", flower6Function6);
+	
+	function flower6Function(){
+		flower6.style.animationDuration= "4s";
+		flower6.style.animationName = "spin";
+		flower6.style.animationIterationCount = "infinite";
+		flower6.style.animationPlayState = "running";
+		flower6.style.animationTimingFunction = "linear";
+	}
+	function flower6Function6(){
+		flower6.style.animationPlayState = "paused";
+	}
+
+star7.addEventListener("click", handleSt7Click)
+
+function handleSt7Click() {
+	flower7.style.display = "block";
+	star7.style.display = "none";
+}
+
+flower7.addEventListener("click", handleFl7Click)
+
+function handleFl7Click() {
+	flower7.style.display = "none";
+	star7.style.display = "block";
+}
+flower7.addEventListener("mouseover", flower7Function);
+	
+flower7.addEventListener("mouseout", flower7Function7);
+	
+	function flower7Function(){
+		flower7.style.animationDuration= "4s";
+		flower7.style.animationName = "spin";
+		flower7.style.animationIterationCount = "infinite";
+		flower7.style.animationPlayState = "running";
+		flower7.style.animationTimingFunction = "linear";
+	}
+	function flower7Function7(){
+		flower7.style.animationPlayState = "paused";
+	}
+
+star8.addEventListener("click", handleSt8Click)
+
+function handleSt8Click() {
+	flower8.style.display = "block";
+	star8.style.display = "none";
+}
+
+flower8.addEventListener("click", handleFl8Click)
+
+function handleFl8Click() {
+	flower8.style.display = "none";
+	star8.style.display = "block";
+}
+
+flower8.addEventListener("mouseover", flower8Function);
+	
+flower8.addEventListener("mouseout", flower8Function8);
+	
+	function flower8Function(){
+		flower8.style.animationDuration= "4s";
+		flower8.style.animationName = "spin";
+		flower8.style.animationIterationCount = "infinite";
+		flower8.style.animationPlayState = "running";
+		flower8.style.animationTimingFunction = "linear";
+	}
+	function flower8Function8(){
+		flower8.style.animationPlayState = "paused";
+	}
+
+star9.addEventListener("click", handleSt9Click)
+
+function handleSt9Click() {
+	flower9.style.display = "block";
+	star9.style.display = "none";
+}
+
+flower9.addEventListener("click", handleFl9Click)
+
+function handleFl9Click() {
+	flower9.style.display = "none";
+	star9.style.display = "block";
+}
+
+flower9.addEventListener("mouseover", flower9Function);
+	
+flower9.addEventListener("mouseout", flower9Function9);
+	
+	function flower9Function(){
+		flower9.style.animationDuration= "4s";
+		flower9.style.animationName = "spin";
+		flower9.style.animationIterationCount = "infinite";
+		flower9.style.animationPlayState = "running";
+		flower9.style.animationTimingFunction = "linear";
+	}
+	function flower9Function9(){
+		flower9.style.animationPlayState = "paused";
+	}
+
+star10.addEventListener("click", handleSt10Click)
+
+function handleSt10Click() {
+	flower10.style.display = "block";
+	star10.style.display = "none";
+}
+
+flower10.addEventListener("click", handleFl10Click)
+
+function handleFl10Click() {
+	flower10.style.display = "none";
+	star10.style.display = "block";
+}
+
+flower10.addEventListener("mouseover", flower10Function);
+	
+flower10.addEventListener("mouseout", flower10Function10);
+	
+	function flower10Function(){
+		flower10.style.animationDuration= "4s";
+		flower10.style.animationName = "spin";
+		flower10.style.animationIterationCount = "infinite";
+		flower10.style.animationPlayState = "running";
+		flower10.style.animationTimingFunction = "linear";
+	}
+	function flower10Function10(){
+		flower10.style.animationPlayState = "paused";
+	}
+
+
+star11.addEventListener("click", handleSt11Click)
+
+function handleSt11Click() {
+	flower11.style.display = "block";
+	star11.style.display = "none";
+}
+
+flower11.addEventListener("click", handleFl11Click)
+
+function handleFl11Click() {
+	flower11.style.display = "none";
+	star11.style.display = "block";
+}
+
+flower11.addEventListener("mouseover", flower11Function);
+	
+flower11.addEventListener("mouseout", flower11Function11);
+	
+	function flower11Function(){
+		flower11.style.animationDuration= "4s";
+		flower11.style.animationName = "spin";
+		flower11.style.animationIterationCount = "infinite";
+		flower11.style.animationPlayState = "running";
+		flower11.style.animationTimingFunction = "linear";
+	}
+	function flower11Function11(){
+		flower11.style.animationPlayState = "paused";
+	}
